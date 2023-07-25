2e7147f85cb650ca4910b6bf6c0deb8b6d70a231
jay-ci <jeancharlesvalentin13@gmail.com>
2023-07-25 09:38:42 +0200

    Header terminé

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Banner from './components/Banner';
+import { BrowserRouter } from 'react-router-dom';
+import Nav from "./components/Nav/Nav"
 import "./utils/main.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Banner/>
-  </React.StrictMode>
+    <Nav/>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
