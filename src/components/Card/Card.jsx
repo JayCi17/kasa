cc8a14f47dba765b592d6d4d8c1a8e670502b344
jay-ci <jeancharlesvalentin13@gmail.com>
2023-07-31 17:42:42 +0200

    page about terminée

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,14 @@
 
 
-function Card ({cover, title}){
-    return(
-    <article className="card">
-        <img src={cover} alt="logement"/>
-        <div className="logement-caption">
-            <p className="title">{title}</p>
-        </div>
-    </article>
+
+function Card({ cover, title }) {
+    return (
+        <article className="card">
+            <img src={cover} alt="logement" />
+            <div className="logement-caption">
+                <p className="title">{title}</p>
+            </div>
+        </article>
     )
 }
 
