e3f5652535a154312e29a05a1124361e3a364b4d
jay-ci <jeancharlesvalentin13@gmail.com>
2023-08-01 00:24:43 +0200

    changement collapse

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,21 +3,21 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/footer";
 import aboutBanner from "../../assets/aboutBanner.png";
 import aboutArray from "../../Datas/aboutArray.json"
-import Collapse from "../../components/collapse";
+import Collapse from "../../components/Collapse/collapse"
 
 
-function About (){
-    return(
+function About() {
+    return (
         <>
-        <div className="en-tête">
-            <Header />
-        </div>
-        <div className="aboutBanner">
-            <img src={aboutBanner} className="about-picture" alt="landscape"/>
-        </div>
-        {aboutArray.map((rule, id)=>(
-            <Collapse key={id} aboutTitle={rule.aboutTitle} aboutText={rule.aboutText} aboutStyle="about-style"/>
-        ))}
+            <div className="en-tête">
+                <Header />
+            </div>
+            <div className="aboutBanner">
+                <img src={aboutBanner} className="about-picture" alt="landscape" />
+            </div>
+            {aboutArray.map((rule, id) => (
+                <Collapse key={id} aboutTitle={rule.aboutTitle} aboutText={rule.aboutText} aboutStyle="about-style" />
+            ))}
             <Footer />
         </>
     )
