bca6bcf24712f0652cd1bc7166e3404221724e9c
jay-ci <jeancharlesvalentin13@gmail.com>
2023-08-06 16:54:08 +0200

    modification du sass pour le responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
 
     return (
         <>
-            <div>
+            <div className="slogan-container">
                 <Slogan />
             </div>
             <div className="card-container">
